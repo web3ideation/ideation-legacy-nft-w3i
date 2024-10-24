39838df46315e7626d5858b11b0c2deaa276a7d6
WolfgangNI <wolfgang.NFTideation@gmail.com>
2024-10-23 19:19:07 -0600

    rename for workflowtest

--- src/LegacyNFT.sol
+++ src/LegacyNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SimpleERC721 is ERC721, Ownable {
+contract LegacyNFT is ERC721, Ownable {
     constructor() ERC721("SimpleNFT", "SNFT") Ownable(msg.sender) {}
 
     function mint(address to, uint256 tokenId) public onlyOwner {
